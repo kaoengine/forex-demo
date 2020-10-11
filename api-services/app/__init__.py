0d5112bb97114463c34cc5ed5472552b02e58ad3
Khanh <tran_khanh@outlook.com>
2020-10-11 15:47:05 +0700

    update

--- api-services/app/__init__.py
+++ api-services/app/__init__.py
@@ -1,6 +1,10 @@
 import os
 from flask import Flask
+from flask import jsonify
+
+
 app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 def main():
@@ -9,6 +13,42 @@ def main():
 @app.route('/how are you')
 def hello():
     return 'I am good, how about you?'
+
+@app.route('/forex')
+def forex():
+    data = {'name': 'Alice', 'datasets': [
+            {
+              'data': [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+              'label': "Africa",
+              'borderColor': "#3e95cd",
+              'fill': 'false',
+            },
+            {
+              'data': [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+              'label': "Asia",
+              'borderColor': "#8e5ea2",
+              'fill': 'false',
+            },
+            {
+              'data': [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+              'label': "Europe",
+              'borderColor': "#3cba9f",
+              'fill': 'false',
+            },
+            {
+              'data': [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+              'label': "Latin America",
+              'borderColor': "#e8c3b9",
+              'fill': 'false',
+            },
+            {
+              'data': [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+              'label': "North America",
+              'borderColor': "#c45850",
+              'fill': 'false',
+            },
+          ]}
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
